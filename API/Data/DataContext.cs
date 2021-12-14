29d46359900380e3730921ad4654af14342d53d2
Calin Corcotoi <calin.corcotoi@gmail.com>
2021-12-14 21:11:33 +0200

    angular is hard

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,19 +1,14 @@
-﻿using API.Entities;
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API.Data
 {
-	public class DataContext:DbContext
-	{
-		public DataContext(DbContextOptions options):base(options)
-		{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
 
-		}
-
-		public DbSet<AppUser> Users { get; set; }
-	}
+        public DbSet<AppUser> Users { get; set; }
+    }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,60 +12,48 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API
 {
-	public class Startup
-	{
-		private readonly IConfiguration _config;
+    public class Startup
+    {
+        private readonly IConfiguration _config;
 
-		public Startup(IConfiguration config)
-		{
-			_config = config;
-		}
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
-		public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddControllers();
+            services.AddCors();
+        }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-			services.AddDbContext<DataContext>(options =>
-			{
-				options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-			});
-			services.AddControllers();
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-			});
-		}
+            app.UseHttpsRedirection();
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-			}
+            app.UseRouting();
 
-			app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-			app.UseRouting();
+            app.UseAuthorization();
 
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
-		}
-	}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
