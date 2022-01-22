11cca5ff52fdd5f560f9c5e3cd870740652d1636
Calin Corcotoi <calin.corcotoi@gmail.com>
2022-01-23 01:44:26 +0200

    14 Adding the Like User feature

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -30,11 +30,11 @@ export class ErrorInterceptor implements HttpInterceptor {
                 }
                 throw modalStateErrors.flat();
               }else{
-                this.toastr.error(error.message, error.status);
+                this.toastr.error(error.error);
               }
               break;
             case 401:
-              this.toastr.error(error.message, error.status);
+              this.toastr.error(error.message, error.error);
               break;
             case 404:
                 this.router.navigateByUrl('/not-found');
